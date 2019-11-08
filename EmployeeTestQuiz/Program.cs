d73674580e0055abc9dc1e654f3687155c66b5bd
Ebenezer Obaseki <ebenibas@outlook.com>
2019-11-08 12:10:36 -0500

    Add project files.

---
+++ EmployeeTestQuiz/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeTestQuiz
+{
+    public interface IDataAccess
+    {
+        string GetEmplyeeSalary(int id);
+        string GetTopThreeSalaries(int id);
+
+    }
+    public class DataAccess : IDataAccess
+    {
+        TestEntities db = new TestEntities();
+
+        public string GetEmployeeSalary(int id)
+        {
+            var employee = db.Employees.Find(id);
+            return employee.Salary.ToString();
+        }
+
+        public string GetEmplyeeSalary(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetTopThreeSalaries(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+
+
+        public string GetTopThreeSalaries()
+
+        {
+            var employee = db.Employees.ToList();
+            var em = employee.OrderByDescending(i => i.Salary).Take(3);
+                return em.ToString();
+        }
+    }
+    public class MoviesBusinessLogic
+    {
+        IDataAccess dta;
+        public MoviesBusinessLogic(IDataAccess dta)
+        {
+            this.dta = dta;
+        }
+        public string GetEmployeeSalary(int id)
+        {
+            return dta.GetEmplyeeSalary(id);
+        }
+
+        public double AnnualBonus(int id)
+        {
+            var salary = dta.GetEmplyeeSalary(id);
+            var bonus = Convert.ToDouble(1 / 100) ;
+            double bonussalry = Convert.ToDouble(salary) + bonus;
+            return bonussalry;
+        }
+        public string TopThreeEmployeeWithSalary(int Id)
+        {
+            return dta.GetTopThreeSalaries(Id);
+        }
+        public string AllInfo(int id)
+        {
+            var result = String.Format("The employees  with salaries {0}, with annual bonuses {1},with topthree employees salaries{2}"
+                , GetEmployeeSalary(id), AnnualBonus(id), TopThreeEmployeeWithSalary(id));
+            return result;
+        }
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+       
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using EmployeeTestQuiz;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        Mock<IDataAccess> fakeDataAccess;
+        MoviesBusinessLogic mbl;
+        [TestInitialize]
+        public void SetUp()
+        {
+            fakeDataAccess = new Mock<IDataAccess>();
+            fakeDataAccess.Setup(m => m.GetEmplyeeSalary(It.IsAny<int>())).Returns("Argo");
+            fakeDataAccess.Setup(m => m.GetTopThreeSalaries(It.IsAny<int>())).Returns("Action");
+            mbl = new MoviesBusinessLogic(fakeDataAccess.Object);
+        }
+        [TestMethod]
+        public void PrintMovieShortInfo_Test_WithMoq()
+        {
+            //Arrange (Handled by Initialize)                
+            //Act
+            var printResult = mbl.AllInfo(5);
+            //Assert
+            Assert.AreEqual(printResult, "The employee Argo with salary Action");
+        }
+    }
+}
